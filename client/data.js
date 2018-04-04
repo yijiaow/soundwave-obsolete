bba5b6d9f88e3e3b496eaaf4d25d7a7e712ff01e
Yijiao Wang <ttwyj250@gmail.com>
2018-04-04 11:44:56 -0700

    Serve popular festivals' data from a separate file. #2

---
+++ client/data.js
@@ -0,0 +1,58 @@
+export const festivals = [
+  {
+    title: 'Coachella',
+    dateRange: 'April 13-15 & April 20-22, 2018',
+    region: '',
+    genre: '',
+    description: '',
+    imageSrc: 'http://localhost:3000/images/coachella.jpg'
+  },
+  {
+    title: 'Sasquatch!',
+    dateRange: 'May 25-27, 2018',
+    region: 'George, WA',
+    genre: '',
+    description: '',
+    imageSrc: 'http://localhost:3000/images/sasquatch.jpeg'
+  },
+  {
+    title: 'Electric Daisy Carnival',
+    dateRange: 'May 18-20, 2018',
+    region: 'Las Vegas, NV',
+    genre: '',
+    description: '',
+    imageSrc: 'http://localhost:3000/images/edc.jpg'
+  },
+  {
+    title: 'Bonnaroo',
+    dateRange: 'June 6-10, 2018',
+    region: 'Manchester, TN',
+    genre: '',
+    description: '',
+    imageSrc: 'http://localhost:3000/images/bonnaroo.jpeg'
+  },
+  {
+    title: 'Newport Jazz',
+    dateRange: 'August 3-5, 2018',
+    region: 'Newport, RI',
+    genre: '',
+    description: '',
+    imageSrc: 'http://localhost:3000/images/newport-jazz.jpeg'
+  },
+  {
+    title: 'Governors Ball',
+    dateRange: 'June 1-3, 2018',
+    region: 'New York, NY',
+    genre: '',
+    description: '',
+    imageSrc: 'http://localhost:3000/images/governors-ball.jpeg'
+  },
+  {
+    title: 'Firefly',
+    dateRange: 'June 14-17, 2018',
+    region: 'Dover, DE',
+    genre: '',
+    description: '',
+    imageSrc: 'http://localhost:3000/images/firefly.jpeg'
+  }
+]
