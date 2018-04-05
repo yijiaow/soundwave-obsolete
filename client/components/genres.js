b9b5afe58bb94fd6275aebfb2295c2f181c84b03
Yijiao Wang <ttwyj250@gmail.com>
2018-04-05 14:09:31 -0700

    Fetch all genres from server when select More Genres. #6

--- client/components/genres.js
+++ client/components/genres.js
@@ -5,33 +5,47 @@ const styles = {
   root: {
     display: 'flex',
     flexDirection: 'row',
+    justifyContent: 'center',
     width: '100%'
   },
   circle: {
-    width: 100,
-    height: 100,
-    margin: 20,
+    width: 85,
+    height: 85,
+    margin: 10,
     textAlign: 'center',
-    borderRadius: 50,
+    fontSize: 14,
+    borderRadius: 40,
     backgroundColor: 'silver'
   },
   selected: {
-    width: 100,
-    height: 100,
-    margin: 20,
+    width: 85,
+    height: 85,
+    margin: 10,
     textAlign: 'center',
-    borderRadius: 50,
+    fontSize: 14,
+    borderRadius: 40,
     backgroundColor: 'tomato'
   }
 }
 export class Genres extends Component {
   constructor(props) {
     super(props)
-    this.state = { selectedIndex: -1, selectedKey: null }
+    this.state = { selectedIndex: -1, selectedKey: null, allGenres: [] }
     this.handleChange = this.handleChange.bind(this)
+    this.fetchGenres = this.fetchGenres.bind(this)
   }
   handleChange(index, key) {
     this.setState({ selectedIndex: index, selectedKey: key })
+  }
+  fetchGenres() {
+    fetch('/genres/all')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ allGenres: data })
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
   render() {
     return (
@@ -39,13 +53,17 @@ export class Genres extends Component {
         {this.props.genres.map((genre, i) => (
           <ListItem
             key={genre.id}
-            primaryText={genre.name}
             style={
               this.state.selectedIndex === i ? styles.selected : styles.circle
             }
             onClick={() => this.handleChange(i, genre.id)}
-          />
+          >
+            <p>{genre.name}</p>
+          </ListItem>
         ))}
+        <ListItem key="more" style={styles.circle} onClick={this.fetchGenres}>
+          <p>More genres</p>
+        </ListItem>
       </List>
     )
   }
