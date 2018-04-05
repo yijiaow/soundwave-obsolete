81d8ce9e5052ea181c2bb471d2b140bb07628328
Yijiao Wang <ttwyj250@gmail.com>
2018-04-05 15:08:16 -0700

    Query filtered events by genre selected and render results on page. #6

--- client/components/genres.js
+++ client/components/genres.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { List, ListItem } from 'material-ui/List'
 
+import { Events } from './events.js'
+
 const styles = {
   root: {
     display: 'flex',
@@ -30,9 +32,15 @@ const styles = {
 export class Genres extends Component {
   constructor(props) {
     super(props)
-    this.state = { selectedIndex: -1, selectedKey: null, allGenres: [] }
+    this.state = {
+      selectedIndex: -1,
+      selectedKey: null,
+      allGenres: [],
+      searchResults: []
+    }
     this.handleChange = this.handleChange.bind(this)
     this.fetchGenres = this.fetchGenres.bind(this)
+    this.renderEvents = this.renderEvents.bind(this)
   }
   handleChange(index, key) {
     this.setState({ selectedIndex: index, selectedKey: key })
@@ -47,24 +55,41 @@ export class Genres extends Component {
         console.log(err)
       })
   }
+  fetchEventsByGenre(id) {
+    fetch(`/events/search?classificationId=${id}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        this.setState({ searchResults: data.events })
+      })
+  }
+  renderEvents(events) {
+    return events.length > 0 ? <Events events={events} /> : null
+  }
   render() {
     return (
-      <List style={styles.root}>
-        {this.props.genres.map((genre, i) => (
-          <ListItem
-            key={genre.id}
-            style={
-              this.state.selectedIndex === i ? styles.selected : styles.circle
-            }
-            onClick={() => this.handleChange(i, genre.id)}
-          >
-            <p>{genre.name}</p>
+      <div>
+        <List style={styles.root}>
+          {this.props.genres.map((genre, i) => (
+            <ListItem
+              key={genre.id}
+              style={
+                this.state.selectedIndex === i ? styles.selected : styles.circle
+              }
+              onClick={() => {
+                this.handleChange(i, genre.id)
+                this.fetchEventsByGenre(genre.id)
+              }}
+            >
+              <p>{genre.name}</p>
+            </ListItem>
+          ))}
+          <ListItem key="more" style={styles.circle} onClick={this.fetchGenres}>
+            <p>More genres</p>
           </ListItem>
-        ))}
-        <ListItem key="more" style={styles.circle} onClick={this.fetchGenres}>
-          <p>More genres</p>
-        </ListItem>
-      </List>
+        </List>
+        {this.renderEvents(this.state.searchResults)}
+      </div>
     )
   }
 }
